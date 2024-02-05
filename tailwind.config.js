92a305a22e72fdf2abbee2949189db7d0f919adb
nathAd17 <natanady7@gmail.com>
2024-02-06 00:26:55 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  content: ["index.html", "node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
