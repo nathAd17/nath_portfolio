be148ab435cbb5098f957ddbb67c4131a3cd4839
nathAd17 <natanady7@gmail.com>
2024-02-17 17:08:51 +0800

    update package.json

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Dark Mode Toggle
-    const darkToggle = document.querySelector("#dark-toggle");
-    const html = document.querySelector("html");
+// mulai dark mode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-    darkToggle.addEventListener("change", function () {
-        if (darkToggle.checked) {
-            html.classList.add("dark");
-        } else {
-            html.classList.remove("dark");
-        }
-    });
+darkToggle.addEventListener("click", function () {
+    if (darkToggle.checked) {
+        html.classList.add("dark");
+    } else {
+        html.classList.remove("dark");
+    }
+});
+// akhir dark mode
 
-    // Mobile Menu Toggle
-    const mobileMenuToggle = document.querySelector('[data-collapse-toggle="mobile-menu"]');
-    const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenuToggle.addEventListener('click', function () {
-        mobileMenu.classList.toggle('hidden');
-    });
-
-    // Back to Top Button
-    const backToTop = document.getElementById('backToTopBtn');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-            backToTop.classList.remove('hidden');
-        } else {
-            backToTop.classList.add('hidden');
-        }
-    });
-
-    backToTop.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+const backToTop = document.getElementById('backToTopBtn');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        backToTop.classList.remove('hidden');
+    } else {
+        backToTop.classList.add('hidden');
+    }
+});
+backToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
